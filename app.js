4bf25518c44a662f95bdb7f8acc0d70c0570cf96
Mimoriam <mimoriam420@gmail.com>
2022-04-11 05:30:26 +0500

    Commit #10 - Register and Login work with JWT (Sending tokens only)

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ connectToDB().then();
 const indexRouter = require('./routes/index');
 const bootcampRouter = require('./routes/bootcamps');
 const courseRouter = require('./routes/courses');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -37,6 +38,7 @@ app.use(fileUpload());
 app.use('/', indexRouter);
 app.use('/api/v1/bootcamps', bootcampRouter);
 app.use('/api/v1/courses', courseRouter);
+app.use('/api/v1/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
